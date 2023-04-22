625bee610fd05ebcb586f41e67fb4a76bf04f126
Arjunarunachalan <119097989+Arjunarunachalan@users.noreply.github.com>
2023-04-22 12:40:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var fileUpload=require('express-fileupload')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout',partialsDir:__dirname+'/views/partials'}))
+app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/Layout',partialsDir:__dirname+'/views/partials'}))
 
 app.use(logger('dev'));
 app.use(express.json());
